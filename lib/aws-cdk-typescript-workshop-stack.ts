231fc20de18c750fd13d0335215398be3306fc7c
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-09-03 15:27:10 +0300

    Create hello lambda

--- lib/aws-cdk-typescript-workshop-stack.ts
+++ lib/aws-cdk-typescript-workshop-stack.ts
@@ -1,7 +1,14 @@
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda')
 
 export class AwsCdkTypescriptWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
   }
 }
