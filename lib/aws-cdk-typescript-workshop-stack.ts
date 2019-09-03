a0c1aa968febc40a264dd311d30a833a17b18f38
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-09-03 15:50:26 +0300

    Create api gateway

--- lib/aws-cdk-typescript-workshop-stack.ts
+++ lib/aws-cdk-typescript-workshop-stack.ts
@@ -1,5 +1,7 @@
 import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda')
+import apigw = require('@aws-cdk/aws-apigateway')
+
 
 export class AwsCdkTypescriptWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -9,6 +11,10 @@ export class AwsCdkTypescriptWorkshopStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_10_X,
       handler: 'hello.handler',
       code: lambda.Code.fromAsset('lambda')
+    })
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     })
   }
 }
